73c1f58116703fed595a046a10805150fff20ffe
King Arthur <king.art.arthur@gmail.com>
2017-01-21 01:59:25 -0500

    mana bar and character hierarchy

--- Source/Farm/FarmCharacter.h
+++ Source/Farm/FarmCharacter.h
@@ -65,5 +65,10 @@ public:
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	/** Returns FollowCamera subobject **/
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	int health = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	int mana = 100.0f;
 };
 
